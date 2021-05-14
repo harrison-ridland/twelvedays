35b33c1313284e5e03017af08d46a6396c52b8ef
harrison-ridland <83881314+harrison-ridland@users.noreply.github.com>
2021-05-13 19:04:01 -0700

    Update sing_day.R
    made functions sing day and song string

--- R/sing_day.R
+++ R/sing_day.R
@@ -13,11 +13,18 @@
 #' @import purrr
 #'
 #' @export
+song_str <- function(days, phrase, line){
+  if (line != 1 & days == 1){
+    saying = str_c("and ", phrase[days])
+  }else{
+    return(phrase[days])
+  }
+  
+}
 sing_day <- function(dataset, line, phrase_col){
-
   phrases <- dataset %>% pull({{phrase_col}})
-
-  #????
-
-
+  dsing = str_c("On the ",dataset$Day.in.Words[line], " day of Christmas, my true love sent to me:" )
+  singvec = c(dsing, sapply(line:1, function(x) song_str(x, phrase = phrases, line = line))) 
+  singvec = glue::as_glue(singvec)
+return(singvec)
 }
